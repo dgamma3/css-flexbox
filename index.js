f50098efb799769c86041073d5b4af0feca2febe
dgamma3 <dgamma3@gmail.com>
2022-12-03 11:50:18 +1000

    3 files modified

--- index.js
+++ index.js
@@ -9,11 +9,16 @@ listElements.forEach((listElem) => {
 });
 console.log('total width of elem', total);
 console.log('remainder', 960 - total);
-console.log('remainder split into 4', (960 - total) / 4);
+console.log(
+  'remainder split into 5 (growth factor: 2+ 1 + 1 + 1',
+  (960 - total) / 5
+);
+const remainder = (960 - total) / 5;
 
-listElements.forEach((listElem) => {
-  console.log(
-    'width after flex grow of 1',
-    listElem.offsetWidth + (960 - total) / 4
-  );
+const firstLi = document.querySelector('li:first-child');
+
+console.log('width after flex grow of 2', firstLi.offsetWidth + remainder * 2);
+
+document.querySelectorAll('li:not(:first-child)').forEach((listElem) => {
+  console.log('width after flex grow of 1', listElem.offsetWidth + remainder);
 });
